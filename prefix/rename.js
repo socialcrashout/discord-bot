048582bfedf094eb13d0c15904d4ffe3abe31385
socialcrashout <phantomreaper875@gmail.com>
2026-07-27 11:43:43 -0500

    Auto-update: 7/27/2026, 11:43:43 AM

---
+++ prefix/rename.js
@@ -0,0 +1,2 @@
+const { renameCurrentTicket } = require('../ticketSystem');
+module.exports = { name: 'rename', async execute(message, args) { const handled = await renameCurrentTicket(message, args.join(' ')); if (!handled) await message.reply('This command can only be used by ticket staff inside an active ticket.'); } };
