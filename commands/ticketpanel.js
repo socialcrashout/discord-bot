210f0f365c15adb2297af83025075123b7158596
socialcrashout <phantomreaper875@gmail.com>
2026-07-27 12:00:11 -0500

    tickets

--- commands/ticketpanel.js
+++
@@ -1,3 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { panelPayload } = require('../utils/ticketsystem');
-module.exports = { data: new SlashCommandBuilder().setName('ticketpanel').setDescription('Post the ticket panel'), execute: (interaction) => interaction.reply(panelPayload()) };
