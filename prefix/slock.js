a338dfd684f02709935ff4b90f060a880836d02a
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:11:20 -0500

    Auto-update: 7/28/2026, 4:11:20 PM

--- prefix/slock.js
+++ prefix/slock.js
@@ -44,7 +44,7 @@ module.exports = {
             if (logChannel) {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1530775133713731826> Slock Command Used!\n` +
+                        `## <:ShieldCheck:1502514212168274061> Slock Command Used!\n` +
                         `-# **<:sig:1530774414436729012> Used By:** ${message.author}\n` +
                         `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
                         `**<:Dot:1530774492412907721> Channel:** ${channel}\n` +
