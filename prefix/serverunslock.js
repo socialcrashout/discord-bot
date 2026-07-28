51a2ea022bfcc40c7a335fdb406d1fd8b22b50d5
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:14:10 -0500

    Auto-update: 7/28/2026, 4:14:10 PM

--- prefix/serverunslock.js
+++ prefix/serverunslock.js
@@ -50,7 +50,7 @@ module.exports = {
                     `## <:ShieldCheck:1502514212168274061> Serverunlock Command Used!\n` +
                     `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
                     `**<:person:1502514200705105981> Channels Unlocked:** ${unlockedCount}\n` +
-                    `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
+                    `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
                     `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                 )
             );
