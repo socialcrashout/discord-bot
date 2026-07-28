2133b3a1662f959a7054571a76c3235b92c2ead9
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:44:24 -0500

    Auto-update: 7/28/2026, 4:44:24 PM

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -63,10 +63,10 @@ module.exports = {
             if (logChannel) {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1530775133713731826> Slowmode Command Used!\n` +
-                        `-# **<:sig:1530774414436729012> Used By:** ${interaction.user}\n` +
-                        `**<:user:1530778349184618627> Slowmode Set:** ${seconds === 0 ? 'Disabled' : `${seconds}s`}\n` +
-                        `**<:Dot:1530774492412907721> Channel:** ${channel}\n` +
+                        `## <:ShieldCheck:1502514212168274061> Slowmode Command Used!\n` +
+                        `-# **<:sig:1502514350014070795> Used By:** ${interaction.user}\n` +
+                        `**<:person:1502514200705105981> Slowmode Set:** ${seconds === 0 ? 'Disabled' : `${seconds}s`}\n` +
+                        `**<:Dot:1502513706347528213> Channel:** ${channel}\n` +
                         `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
