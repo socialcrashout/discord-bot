85f2c83f6c98391940f1c86fa64628e219ccafac
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:19:22 -0500

    Auto-update: 7/28/2026, 4:19:22 PM

--- prefix/ban.js
+++ prefix/ban.js
@@ -37,7 +37,7 @@ module.exports = {
             if (logChannel) {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1530775133713731826> Ban Command Used! | Case #${caseNumber}\n` +
+                        `## <:ShieldCheck:1502514212168274061> Ban Command Used! | Case #${caseNumber}\n` +
                         `-# **<:sig:1530774414436729012> Used By:** ${message.author}\n` +
                         `**<:user:1530778349184618627> User Banned:** ${target.user.tag} (${target.id})\n` +
                         `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
