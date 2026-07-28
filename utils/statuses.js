84a2cf25f9f6f1e3e3900fb30390aca955ae1823
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 17:19:49 -0500

    Auto-update: 7/28/2026, 5:19:49 PM

--- utils/statuses.js
+++ utils/statuses.js
@@ -3,7 +3,7 @@ const { ActivityType } = require('discord.js');
 // Customize your rotating statuses here
 // type options: Playing, Streaming, Listening, Watching, Competing
 module.exports = [
-  { name: '📊 Optimizing every workflow', type: ActivityType.Playing },
+  { name: '📊 Optimizing every workflow', type: ActivityType.Streaming },
   { name: '🤖 Automating excellence', type: ActivityType.Watching },
   { name: 'Streamlining .mode operations.', type: ActivityType.Listening },
   { name: 'Mi Mug Tea', type: ActivityType.Competing },
