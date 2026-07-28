c417376bf26456991f7fefdf9db0c86cb75193d3
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:04:32 -0500

    Auto-update: 7/28/2026, 4:04:32 PM

--- prefix/unslock.js
+++ prefix/unslock.js
@@ -40,7 +40,7 @@ module.exports = {
                         `## <:ShieldCheck:1502514212168274061> Unslock Command Used!\n` +
                         `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
                         `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
-                        `**<:Dot:1530774492412907721> Channel:** ${channel}\n` +
+                        `**<:Dot:1502513706347528213> Channel:** ${channel}\n` +
                         `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
