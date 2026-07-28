eac2f0b8b7788842a4e36e5b9cac9dc46be95625
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:03:06 -0500

    Auto-update: 7/28/2026, 4:03:06 PM

--- prefix/untimeout.js
+++ prefix/untimeout.js
@@ -41,7 +41,7 @@ module.exports = {
                     new TextDisplayBuilder().setContent(
                         `## <:ShieldCheck:1502514212168274061> Untimeout Command Used! | Case #${caseNumber}\n` +
                         `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
-                        `**<:user:1530778349184618627> User Untimed Out:** ${target.user.tag} (${target.id})\n` +
+                        `**<:person:1502514200705105981> User Untimed Out:** ${target.user.tag} (${target.id})\n` +
                         `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
                         `**<:Dot:1530774492412907721> Channel:** ${message.channel}\n` +
                         `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
