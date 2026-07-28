d8506886cdb64d035112467d71454d02e11c7867
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:56:30 -0500

    Auto-update: 7/28/2026, 4:56:30 PM

--- commands/purje.js
+++ commands/purje.js
@@ -92,12 +92,12 @@ module.exports = {
             if (logChannel) {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1530775133713731826> Purge Command Used!\n` +
-                        `-# **<:sig:1530774414436729012> Used By:** ${interaction.user}\n` +
-                        `**<:user:1530778349184618627> Messages Deleted:** ${deletedCount}${target ? ` (from ${target.tag})` : ''}\n` +
-                        `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
-                        `**<:Dot:1530774492412907721> Channel:** ${interaction.channel}\n` +
-                        `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                        `## <:ShieldCheck:1502514212168274061> Purge Command Used!\n` +
+                        `-# **<:sig:1502514350014070795> Used By:** ${interaction.user}\n` +
+                        `**<:person:1502514200705105981> Messages Deleted:** ${deletedCount}${target ? ` (from ${target.tag})` : ''}\n` +
+                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
+                        `**<:Dot:1502513706347528213> Channel:** ${interaction.channel}\n` +
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
 
