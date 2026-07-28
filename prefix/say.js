1a6a328936bb2858b15a453a947b917cbf61bd86
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 15:47:38 -0500

    Auto-update: 7/28/2026, 3:47:38 PM

--- prefix/say.js
+++ prefix/say.js
@@ -1,7 +1,7 @@
 const { ContainerBuilder, TextDisplayBuilder, MessageFlags } = require('discord.js');
 
 const ALLOWED_ROLE_ID = '1529922817578106961';
-const LOG_CHANNEL_ID = '1529922818253390018';
+const LOG_CHANNEL_ID = '1506450870269906944';
 
 module.exports = {
   name: "say",
