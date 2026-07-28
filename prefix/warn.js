0ba4928f4e429797a1f4dd84fa079ec76dc65105
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:01:05 -0500

    Auto-update: 7/28/2026, 4:01:05 PM

--- prefix/warn.js
+++ prefix/warn.js
@@ -49,7 +49,7 @@ module.exports = {
                         `**<:person:1502514200705105981> User Warned:** ${target.user.tag} (${target.id})\n` +
                         `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
                         `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
-                        `**<:Calendar:1530778367966843010> Timestamp:** <t:${timestamp}:F>`
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${timestamp}:F>`
                     )
                 );
 
