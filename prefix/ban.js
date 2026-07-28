40a62ae36ba5b3f69d1d1773cdd14360ffe06ce4
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:20:32 -0500

    Auto-update: 7/28/2026, 4:20:32 PM

--- prefix/ban.js
+++ prefix/ban.js
@@ -41,8 +41,8 @@ module.exports = {
                         `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
                         `**<:person:1502514200705105981> User Banned:** ${target.user.tag} (${target.id})\n` +
                         `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
-                        `**<:Dot:1530774492412907721> Channel:** ${message.channel}\n` +
-                        `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                        `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
 
