20479f5bab0e805d25db366995533583e1eaae8d
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:06:10 -0500

    Auto-update: 7/28/2026, 4:06:10 PM

--- prefix/unban.js
+++ prefix/unban.js
@@ -48,7 +48,7 @@ module.exports = {
                         `**<:person:1502514200705105981> User Unbanned:** ${userTag} (${rawId})\n` +
                         `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
                         `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
-                        `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
 
