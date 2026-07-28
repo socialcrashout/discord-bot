67d5a32a9b5cd2219b4046f7934c0ead5ac4aba4
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:18:01 -0500

    Auto-update: 7/28/2026, 4:18:01 PM

--- prefix/kick.js
+++ prefix/kick.js
@@ -39,9 +39,9 @@ module.exports = {
                     new TextDisplayBuilder().setContent(
                         `## <:ShieldCheck:1502514212168274061> Kick Command Used! | Case #${caseNumber}\n` +
                         `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
-                        `**<:user:1530778349184618627> User Kicked:** ${target.user.tag} (${target.id})\n` +
-                        `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
-                        `**<:Dot:1530774492412907721> Channel:** ${message.channel}\n` +
+                        `**<:person:1502514200705105981> User Kicked:** ${target.user.tag} (${target.id})\n` +
+                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
+                        `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
                         `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
