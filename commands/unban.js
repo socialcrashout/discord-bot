1b840177b8d7622894a0e98931baf0c46ea3a40f
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:40:09 -0500

    Auto-update: 7/28/2026, 4:40:09 PM

--- commands/unban.js
+++ commands/unban.js
@@ -54,12 +54,12 @@ module.exports = {
             if (logChannel) {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1530775133713731826> Unban Command Used! | Case #${caseNumber}\n` +
-                        `-# **<:sig:1530774414436729012> Used By:** ${interaction.user}\n` +
-                        `**<:user:1530778349184618627> User Unbanned:** ${userTag} (${userId})\n` +
-                        `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
-                        `**<:Dot:1530774492412907721> Channel:** ${interaction.channel}\n` +
-                        `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                        `## <:ShieldCheck:1502514212168274061> Unban Command Used! | Case #${caseNumber}\n` +
+                        `-# **<:sig:1502514350014070795> Used By:** ${interaction.user}\n` +
+                        `**<:person:1502514200705105981> User Unbanned:** ${userTag} (${userId})\n` +
+                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
+                        `**<:Dot:1502513706347528213> Channel:** ${interaction.channel}\n` +
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
 
