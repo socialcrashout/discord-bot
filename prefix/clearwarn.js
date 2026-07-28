47bad965d7e7e2c4613501c51845640fd617eaaf
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:18:43 -0500

    Auto-update: 7/28/2026, 4:18:43 PM

--- prefix/clearwarn.js
+++ prefix/clearwarn.js
@@ -36,7 +36,7 @@ module.exports = {
                 new TextDisplayBuilder().setContent(
                     `## <:ShieldCheck:1502514212168274061> Clearwarn Command Used!\n` +
                     `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
-                    `**<:user:1530778349184618627> Warning Removed:** Case #${caseNumber} (was ${removed.userTag})\n` +
+                    `**<:person:1502514200705105981> Warning Removed:** Case #${caseNumber} (was ${removed.userTag})\n` +
                     `**<:Comment:1530774457961025618> Original Reason:** ${removed.reason}\n` +
                     `**<:Dot:1530774492412907721> Channel:** ${message.channel}\n` +
                     `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
