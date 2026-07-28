bcb05866328c451f84b3fc72565a1397541f495f
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:36:23 -0500

    Auto-update: 7/28/2026, 4:36:23 PM

--- commands/unslock.js
+++ commands/unslock.js
@@ -44,10 +44,10 @@ module.exports = {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
                         `## <:ShieldCheck:1502514212168274061> Unslock Command Used!\n` +
-                        `-# **<:sig:1530774414436729012> Used By:** ${interaction.user}\n` +
-                        `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
-                        `**<:Dot:1530774492412907721> Channel:** ${channel}\n` +
-                        `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                        `-# **<:sig:1502514350014070795> Used By:** ${interaction.user}\n` +
+                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
+                        `**<:Dot:1502513706347528213> Channel:** ${channel}\n` +
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
                     )
                 );
 
