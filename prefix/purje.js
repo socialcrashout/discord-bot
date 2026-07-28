5635684cb36d2a583c5f5c7c15b4c8411638bfb7
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:16:40 -0500

    Auto-update: 7/28/2026, 4:16:40 PM

--- prefix/purje.js
+++ prefix/purje.js
@@ -81,9 +81,9 @@ module.exports = {
             if (logChannel) {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1530775133713731826> Purge Command Used!\n` +
-                        `-# **<:sig:1530774414436729012> Used By:** ${message.author}\n` +
-                        `**<:user:1530778349184618627> Messages Deleted:** ${deletedCount}${target ? ` (from ${target.tag})` : ''}\n` +
+                        `## <:ShieldCheck:1502514212168274061> Purge Command Used!\n` +
+                        `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
+                        `**<:person:1502514200705105981> Messages Deleted:** ${deletedCount}${target ? ` (from ${target.tag})` : ''}\n` +
                         `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
                         `**<:Dot:1530774492412907721> Channel:** ${message.channel}\n` +
                         `**<:Calendar:1530778367966843010> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
