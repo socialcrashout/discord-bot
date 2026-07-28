3ae7a6bc152f0b7c4dc251fc02dc23c6b59aecaa
socialcrashout <phantomreaper875@gmail.com>
2026-07-28 16:00:45 -0500

    Auto-update: 7/28/2026, 4:00:45 PM

--- prefix/warn.js
+++ prefix/warn.js
@@ -47,7 +47,7 @@ module.exports = {
                         `## <:ShieldCheck:1502514212168274061> Warn Command Used! | Case #${caseNumber}\n` +
                         `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
                         `**<:person:1502514200705105981> User Warned:** ${target.user.tag} (${target.id})\n` +
-                        `**<:Comment:1530774457961025618> Reason:** ${reason}\n` +
+                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
                         `**<:Dot:1530774492412907721> Channel:** ${message.channel}\n` +
                         `**<:Calendar:1530778367966843010> Timestamp:** <t:${timestamp}:F>`
                     )
