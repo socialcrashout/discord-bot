43f4ef68107bcfd7ff4745858ea45e60ae2f6f81
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:08:05 -0500

    Auto-update: 7/29/2026, 4:08:05 PM

--- prefix/funds.js
+++ prefix/funds.js
@@ -1,4 +1,5 @@
 const { MessageFlags } = require('discord.js');
+
 const {
     hasPermission,
     buildNoPermissionContainer,
@@ -7,9 +8,14 @@ const {
     errorMessage,
 } = require('../utils/fundsHelpers');
 
+const ALLOWED_ROLE_ID = 'ROLE_ID_HERE'; // Replace with the role ID that can use -funds
+
 module.exports = {
     name: 'funds',
+
     execute: async (message, args, client) => {
+
+        // Permission check
         if (!hasPermission(message.member)) {
             return message.reply({
                 components: [buildNoPermissionContainer()],
@@ -17,18 +23,31 @@ module.exports = {
             });
         }
 
+        // Role restriction
+        if (!message.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+            return message.reply({
+                components: [buildNoPermissionContainer('You do not have the required role to use this command.')],
+                flags: MessageFlags.IsComponentsV2,
+            });
+        }
+
         const loadingMsg = await message.reply('💸 Fetching group funds...');
 
         const groupId = process.env.GROUP_ID;
+
         try {
             const data = await fetchFundsData(groupId);
+
             await loadingMsg.edit({
                 content: null,
                 components: [buildFundsContainer(data)],
                 flags: MessageFlags.IsComponentsV2,
             });
+
         } catch (err) {
-            await loadingMsg.edit({ content: errorMessage(err) });
+            await loadingMsg.edit({
+                content: errorMessage(err),
+            });
         }
     },
 };
