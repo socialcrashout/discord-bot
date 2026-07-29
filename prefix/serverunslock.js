ed0d97a8c90022f4337d99616bb4b07330d4b7ce
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:24:36 -0500

    Auto-update: 7/29/2026, 4:24:36 PM

--- prefix/serverunslock.js
+++ prefix/serverunslock.js
@@ -1,30 +1,38 @@
-const { PermissionFlagsBits, ContainerBuilder, TextDisplayBuilder, MessageFlags } = require('discord.js');
+const { ContainerBuilder, TextDisplayBuilder, MessageFlags } = require('discord.js');
 const { getLockState, clearLockState } = require('../utils/serverLock');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
-const ALLOWED_ROLE_ID = '1504311819458580531';
+
+const ALLOWED_ROLE_IDS = [
+    '1504311819458580531'
+];
 
 module.exports = {
     name: 'serverunlock',
     description: 'Unlock every text channel that was locked by -serverslock',
     // Usage: -serverunlock reason...
     async execute(message, args) {
+
         const errorReply = (text) => message.reply({
-            components: [new ContainerBuilder().addTextDisplayComponents(
-                new TextDisplayBuilder().setContent(text)
-            )],
+            components: [
+                new ContainerBuilder().addTextDisplayComponents(
+                    new TextDisplayBuilder().setContent(text)
+                )
+            ],
             flags: MessageFlags.IsComponentsV2,
         });
 
-        if (!message.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+        // Role restriction
+        const hasRole = ALLOWED_ROLE_IDS.some(roleId =>
+            message.member.roles.cache.has(roleId)
+        );
+
+        if (!hasRole) {
             return errorReply('You do not have permission to use this command.');
         }
 
-        if (!message.member.permissions.has(PermissionFlagsBits.ManageChannels)) {
-            return errorReply('You do not have permission to manage channels.');
-        }
-
         const lockState = getLockState(message.guild.id);
+
         if (!lockState) {
             return errorReply('<:WarningIcon:1508245066135765034> The server is not currently locked.');
         }
@@ -44,6 +52,7 @@ module.exports = {
                     { SendMessages: previousValue },
                     { reason }
                 );
+
                 unlockedCount++;
             } catch (err) {
                 console.error(`Failed to unlock channel ${channelId}:`, err);
@@ -55,15 +64,16 @@ module.exports = {
         const logChannel = message.guild.channels.cache.get(LOG_CHANNEL_ID);
 
         if (logChannel) {
-            const logContainer = new ContainerBuilder().addTextDisplayComponents(
-                new TextDisplayBuilder().setContent(
-                    `## <:ShieldCheck:1502514212168274061> Serverunlock Command Used!\n` +
-                    `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
-                    `**<:person:1502514200705105981> Channels Unlocked:** ${unlockedCount}\n` +
-                    `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
-                    `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
-                )
-            );
+            const logContainer = new ContainerBuilder()
+                .addTextDisplayComponents(
+                    new TextDisplayBuilder().setContent(
+                        `## <:ShieldCheck:1502514212168274061> Serverunlock Command Used!\n` +
+                        `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
+                        `**<:person:1502514200705105981> Channels Unlocked:** ${unlockedCount}\n` +
+                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                    )
+                );
 
             await logChannel.send({
                 components: [logContainer],
@@ -72,6 +82,6 @@ module.exports = {
             });
         }
 
-        await message.channel.send(`🔓`);
+        await message.channel.send('🔓');
     },
 };
