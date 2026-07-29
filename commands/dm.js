c0434a1efade8cca9dbf64e2f89750f1df7fbbe9
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:35:36 -0500

    Auto-update: 7/29/2026, 4:35:36 PM

--- commands/dm.js
+++ commands/dm.js
@@ -3,53 +3,76 @@ const { ContainerBuilder, TextDisplayBuilder, MessageFlags } = require('discord.
 
 const LOG_CHANNEL_ID = '1506450870269906944';
 
+const STAFF_ROLE_IDS = [
+    '1504311819458580531'
+];
+
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('dmuser')
-    .setDescription('DM a user and log the interaction')
-    .addUserOption(option => option.setName('user').setDescription('The user to DM').setRequired(true))
-    .addStringOption(option => option.setName('message').setDescription('The message to send').setRequired(true)),
+    data: new SlashCommandBuilder()
+        .setName('dmuser')
+        .setDescription('DM a user and log the interaction')
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('The user to DM')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('message')
+                .setDescription('The message to send')
+                .setRequired(true)
+        ),
 
-  async execute(interaction) {
-    const staffRoleIds = ['1504311819458580531'];
+    async execute(interaction) {
 
-    if (!interaction.member.roles.cache.some(role => staffRoleIds.includes(role.id))) {
-      return interaction.reply({
-        content: "❌ You do not have permission to use this command.",
-        ephemeral: true,
-      });
+        // Role restriction
+        if (!interaction.member.roles.cache.some(role => STAFF_ROLE_IDS.includes(role.id))) {
+            return interaction.reply({
+                content: '❌ You do not have permission to use this command.',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
+
+        const staff = interaction.user;
+        const user = interaction.options.getUser('user');
+        const message = interaction.options.getString('message');
+
+        try {
+            await user.send(message);
+
+            const logChannel = interaction.guild.channels.cache.get(LOG_CHANNEL_ID);
+
+            if (logChannel) {
+                const container = new ContainerBuilder()
+                    .addTextDisplayComponents(
+                        new TextDisplayBuilder().setContent(
+                            `## <:ShieldCheck:1502514212168274061> DM Command Used!\n` +
+                            `-# **<:sig:1502514350014070795> Used by:** ${staff}\n` +
+                            `**<:Comment:1502512880493400196> Sent to:** ${user} (${user.id})\n` +
+                            `**<:Dot:1502513706347528213> Message:** ${message}`
+                        )
+                    );
+
+                await logChannel.send({
+                    components: [container],
+                    flags: MessageFlags.IsComponentsV2,
+                    allowedMentions: { parse: [] },
+                });
+            }
+
+            await interaction.reply({
+                content: `✅ Successfully DM'd ${user.tag} and logged the interaction.`,
+                flags: MessageFlags.Ephemeral,
+            });
+
+        } catch (error) {
+            console.error(error);
+
+            await interaction.reply({
+                content: '❌ There was an error sending the DM or logging the interaction.',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
     }
-
-    const staff = interaction.user;
-    const user = interaction.options.getUser('user');
-    const message = interaction.options.getString('message');
-
-    try {
-      await user.send(message);
-
-      const logChannel = interaction.guild.channels.cache.get(LOG_CHANNEL_ID);
-      if (logChannel) {
-        const container = new ContainerBuilder()
-          .addTextDisplayComponents(
-            new TextDisplayBuilder().setContent(
-              `## <:ShieldCheck:1502514212168274061> DM Command Used!\n` +
-              `-# **<:sig:1502514350014070795> Used by:** ${staff}\n` +
-              `**<:Comment:1502512880493400196> Sent to:** ${user} (${user.id})\n` +
-              `**<:Dot:1502513706347528213> Message:** ${message}`
-            )
-          );
-
-        await logChannel.send({
-          components: [container],
-          flags: MessageFlags.IsComponentsV2,
-          allowedMentions: { parse: [] },
-        });
-      }
-
-      await interaction.reply({ content: `Successfully DM'd ${user.tag} and logged the interaction.`, ephemeral: true });
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'There was an error sending the DM or logging the interaction.', ephemeral: true });
-    }
-  }
 };
