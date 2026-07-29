6bae9aef9e72f98bb43e63408e6f65b2b7151698
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 12:34:26 -0500

    Auto-update: 7/29/2026, 12:34:26 PM

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    banner:
+    "https://yumi.onl/api/files/6a6a38b554d6927723c15003/raw",
+
+    footer:
+    "https://yumi.onl/api/files/6a6974fa91bbc4fb21f03ab5/raw",
+
+    color: "#F74FA7",
+
+    verificationLog:
+    "1532078127084343407",
+
+    roles: {
+        unverified:
+        "1532078230545109254",
+
+        verified:
+        "1504325783634841600"
+    }
+
+};
