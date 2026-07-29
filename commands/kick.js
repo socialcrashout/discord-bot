1b903e3f1da3220f8cf86b9bc7d19909c60ffcd8
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:34:39 -0500

    Auto-update: 7/29/2026, 4:34:39 PM

--- commands/kick.js
+++ commands/kick.js
@@ -10,7 +10,13 @@ const getNextCase = require('../utils/getNextCase');
 const { addModLog } = require('../utils/modlogs');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
-const ALLOWED_ROLE_ID = '1504311819458580531,1504313264576925757,1504312910862880879,1504320706341502996'; // Replace with the role ID that can use /kick
+
+const ALLOWED_ROLE_IDS = [
+    '1504311819458580531',
+    '1504313264576925757',
+    '1504312910862880879',
+    '1504320706341502996'
+];
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -42,7 +48,7 @@ module.exports = {
         }
 
         // Role restriction
-        if (!interaction.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+        if (!interaction.member.roles.cache.some(role => ALLOWED_ROLE_IDS.includes(role.id))) {
             return errorReply('You do not have the required role to use this command.');
         }
 
@@ -79,7 +85,6 @@ module.exports = {
                 timestamp,
             });
 
-            // Log to mod-log channel
             const logChannel = interaction.guild.channels.cache.get(LOG_CHANNEL_ID);
 
             if (logChannel) {
