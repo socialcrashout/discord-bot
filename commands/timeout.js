d6bdd03e1d57c3b81eb0f42fad42781fc02ce145
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:01:34 -0500

    Auto-update: 7/29/2026, 4:01:34 PM

--- commands/timeout.js
+++ commands/timeout.js
@@ -10,7 +10,7 @@ const getNextCase = require('../utils/getNextCase');
 const { addModLog } = require('../utils/modlogs');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
-const ALLOWED_ROLE_ID = 'ROLE_ID_HERE'; // Replace with the role ID that can use /timeout
+const ALLOWED_ROLE_ID = '1504311819458580531,1504313264576925757,1504312910862880879,1504320706341502996'; // Replace with the role ID that can use /timeout
 
 // Parses strings like "10m", "1h", "2d" into milliseconds. Max allowed by Discord is 28 days.
 function parseDuration(input) {
