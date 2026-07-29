cc20759a9e12332e786b92f20a54930eb30fdd9a
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:17:08 -0500

    Auto-update: 7/29/2026, 4:17:08 PM

--- prefix/unslock.js
+++ prefix/unslock.js
@@ -1,7 +1,7 @@
 const { ContainerBuilder, TextDisplayBuilder, MessageFlags } = require('discord.js');
 const { getLock, clearLock } = require('../utils/channelLock');
 
-const ALLOWED_ROLE_ID = 'YOUR_ROLE_ID_HERE';
+const ALLOWED_ROLE_ID = '1504311819458580531,1504313264576925757,1504312910862880879';
 const LOG_CHANNEL_ID = '1506450870269906944';
 
 module.exports = {
