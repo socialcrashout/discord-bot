0e45bc08d79d1c79192e6df1f02d1a99ee50a0e8
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:27:09 -0500

    Auto-update: 7/29/2026, 4:27:09 PM

--- prefix/ban.js
+++ prefix/ban.js
@@ -1,5 +1,4 @@
 const {
-    PermissionFlagsBits,
     ContainerBuilder,
     TextDisplayBuilder,
     MessageFlags,
@@ -9,14 +8,20 @@ const getNextCase = require('../utils/getNextCase');
 const { addModLog } = require('../utils/modlogs');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
-const ALLOWED_ROLE_ID = '1504311819458580531,1504312910862880879'; // Replace with the role ID that can use -ban
+
+const ALLOWED_ROLE_IDS = [
+    '1504311819458580531',
+    '1504312910862880879'
+];
 
 module.exports = {
     name: 'ban',
     description: 'Ban a member from the server',
+
     // Usage: -ban @user reason...
 
     async execute(message, args) {
+
         const errorReply = (text) => message.reply({
             components: [
                 new ContainerBuilder().addTextDisplayComponents(
@@ -26,13 +31,8 @@ module.exports = {
             flags: MessageFlags.IsComponentsV2,
         });
 
-        // Permission check
-        if (!message.member.permissions.has(PermissionFlagsBits.BanMembers)) {
-            return errorReply('You do not have permission to ban members.');
-        }
-
         // Role restriction
-        if (!message.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+        if (!message.member.roles.cache.some(role => ALLOWED_ROLE_IDS.includes(role.id))) {
             return errorReply('You do not have the required role to use this command.');
         }
 
@@ -47,7 +47,9 @@ module.exports = {
         const reason = args.slice(1).join(' ') || 'No reason provided';
 
         if (!target.bannable) {
-            return errorReply('I cannot ban this member. They may have a higher role than me or I lack permissions.');
+            return errorReply(
+                'I cannot ban this member. They may have a higher role than me or I lack permissions.'
+            );
         }
 
         if (target.id === message.author.id) {
@@ -71,20 +73,20 @@ module.exports = {
                 timestamp,
             });
 
-            // Log to mod-log channel
             const logChannel = message.guild.channels.cache.get(LOG_CHANNEL_ID);
 
             if (logChannel) {
-                const logContainer = new ContainerBuilder().addTextDisplayComponents(
-                    new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1502514212168274061> Ban Command Used! | Case #${caseNumber}\n` +
-                        `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
-                        `**<:person:1502514200705105981> User Banned:** ${target.user.tag} (${target.id})\n` +
-                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
-                        `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
-                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${timestamp}:F>`
-                    )
-                );
+                const logContainer = new ContainerBuilder()
+                    .addTextDisplayComponents(
+                        new TextDisplayBuilder().setContent(
+                            `## <:ShieldCheck:1502514212168274061> Ban Command Used! | Case #${caseNumber}\n` +
+                            `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
+                            `**<:person:1502514200705105981> User Banned:** ${target.user.tag} (${target.id})\n` +
+                            `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
+                            `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
+                            `**<:Calendar:1502513561866473734> Timestamp:** <t:${timestamp}:F>`
+                        )
+                    );
 
                 await logChannel.send({
                     components: [logContainer],
@@ -93,14 +95,15 @@ module.exports = {
                 });
             }
 
-            const container = new ContainerBuilder().addTextDisplayComponents(
-                new TextDisplayBuilder().setContent(
-                    `**Member Banned** | Case #${caseNumber}\n` +
-                    `**User:** ${target.user.tag} (${target.id})\n` +
-                    `**Moderator:** ${message.author.tag}\n` +
-                    `**Reason:** ${reason}`
-                )
-            );
+            const container = new ContainerBuilder()
+                .addTextDisplayComponents(
+                    new TextDisplayBuilder().setContent(
+                        `**Member Banned** | Case #${caseNumber}\n` +
+                        `**User:** ${target.user.tag} (${target.id})\n` +
+                        `**Moderator:** ${message.author.tag}\n` +
+                        `**Reason:** ${reason}`
+                    )
+                );
 
             await message.channel.send({
                 components: [container],
