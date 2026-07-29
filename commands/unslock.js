49c5dbfd3bf909e360a29a0f22518ca875b0ff0d
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:29:03 -0500

    Auto-update: 7/29/2026, 4:29:03 PM

--- commands/unslock.js
+++ commands/unslock.js
@@ -1,6 +1,5 @@
 const {
     SlashCommandBuilder,
-    PermissionFlagsBits,
     ContainerBuilder,
     TextDisplayBuilder,
     MessageFlags,
@@ -9,19 +8,26 @@ const {
 const { getLock, clearLock } = require('../utils/channelLock');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
-const ALLOWED_ROLE_ID = '1504311819458580531,1504313264576925757,1504312910862880879'; // Replace with the role ID that can use /unslock
+
+const ALLOWED_ROLE_IDS = [
+    '1504311819458580531',
+    '1504313264576925757',
+    '1504312910862880879'
+];
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('unslock')
         .setDescription('Unlock the channel this command is run in')
         .addStringOption(option =>
-            option.setName('reason')
+            option
+                .setName('reason')
                 .setDescription('Reason for the unlock')
-                .setRequired(false))
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+                .setRequired(false)
+        ),
 
     async execute(interaction) {
+
         const errorReply = (text) => interaction.reply({
             components: [
                 new ContainerBuilder().addTextDisplayComponents(
@@ -31,13 +37,8 @@ module.exports = {
             flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
         });
 
-        // Permission check
-        if (!interaction.member.permissions.has(PermissionFlagsBits.ManageChannels)) {
-            return errorReply('You do not have permission to manage channels.');
-        }
-
         // Role restriction
-        if (!interaction.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+        if (!ALLOWED_ROLE_IDS.some(role => interaction.member.roles.cache.has(role))) {
             return errorReply('You do not have the required role to use this command.');
         }
 
@@ -63,15 +64,16 @@ module.exports = {
             const logChannel = interaction.guild.channels.cache.get(LOG_CHANNEL_ID);
 
             if (logChannel) {
-                const logContainer = new ContainerBuilder().addTextDisplayComponents(
-                    new TextDisplayBuilder().setContent(
-                        `## <:ShieldCheck:1502514212168274061> Unslock Command Used!\n` +
-                        `-# **<:sig:1502514350014070795> Used By:** ${interaction.user}\n` +
-                        `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
-                        `**<:Dot:1502513706347528213> Channel:** ${channel}\n` +
-                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
-                    )
-                );
+                const logContainer = new ContainerBuilder()
+                    .addTextDisplayComponents(
+                        new TextDisplayBuilder().setContent(
+                            `## <:ShieldCheck:1502514212168274061> Unslock Command Used!\n` +
+                            `-# **<:sig:1502514350014070795> Used By:** ${interaction.user}\n` +
+                            `**<:Comment:1502512880493400196> Reason:** ${reason}\n` +
+                            `**<:Dot:1502513706347528213> Channel:** ${channel}\n` +
+                            `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                        )
+                    );
 
                 await logChannel.send({
                     components: [logContainer],
@@ -86,7 +88,10 @@ module.exports = {
 
         } catch (error) {
             console.error(error);
-            await errorReply('Something went wrong while unlocking this channel.');
+
+            await errorReply(
+                'Something went wrong while unlocking this channel.'
+            );
         }
     },
 };
