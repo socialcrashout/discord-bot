6e02cbe521c54e4c2befae8bfd2543acfa7fb06a
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 15:59:55 -0500

    Auto-update: 7/29/2026, 3:59:55 PM

--- commands/slock.js
+++ commands/slock.js
@@ -9,7 +9,7 @@ const {
 const { saveLock, getLock } = require('../utils/channelLock');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
-const ALLOWED_ROLE_ID = 'ROLE_ID_HERE'; // Replace with the role ID that can use /slock
+const ALLOWED_ROLE_ID = '1504311819458580531,1504313264576925757,1504312910862880879'; // Replace with the role ID that can use /slock
 
 module.exports = {
     data: new SlashCommandBuilder()
