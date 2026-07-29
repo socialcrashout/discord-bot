cda4176d80c1372f63f0c08c0d52c0e082d91bde
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 15:54:32 -0500

    Auto-update: 7/29/2026, 3:54:32 PM

--- commands/kick.js
+++ commands/kick.js
@@ -1,8 +1,16 @@
-const { SlashCommandBuilder, PermissionFlagsBits, ContainerBuilder, TextDisplayBuilder, MessageFlags } = require('discord.js');
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    ContainerBuilder,
+    TextDisplayBuilder,
+    MessageFlags,
+} = require('discord.js');
+
 const getNextCase = require('../utils/getNextCase');
 const { addModLog } = require('../utils/modlogs');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
+const ALLOWED_ROLE_ID = '1504311819458580531,1504313264576925757,1504312910862880879,1504320706341502996'; // Replace with the role ID that can use /kick
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -19,29 +27,40 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
 
     async execute(interaction) {
+        const errorReply = (text) => interaction.reply({
+            components: [
+                new ContainerBuilder().addTextDisplayComponents(
+                    new TextDisplayBuilder().setContent(text)
+                )
+            ],
+            flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
+        });
+
+        // Permission check
         if (!interaction.member.permissions.has(PermissionFlagsBits.KickMembers)) {
-            return interaction.reply({
-                components: [new ContainerBuilder().addTextDisplayComponents(
-                    new TextDisplayBuilder().setContent('You do not have permission to kick members.')
-                )],
-                flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
-            });
+            return errorReply('You do not have permission to kick members.');
+        }
+
+        // Role restriction
+        if (!interaction.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+            return errorReply('You do not have the required role to use this command.');
         }
 
         const target = interaction.options.getUser('target');
         const reason = interaction.options.getString('reason') || 'No reason provided';
         const member = interaction.guild.members.cache.get(target.id);
 
-        const errorReply = (text) => interaction.reply({
-            components: [new ContainerBuilder().addTextDisplayComponents(
-                new TextDisplayBuilder().setContent(text)
-            )],
-            flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
-        });
+        if (!member) {
+            return errorReply('That user is not in this server.');
+        }
 
-        if (!member) return errorReply('That user is not in this server.');
-        if (!member.kickable) return errorReply('I cannot kick this member. They may have a higher role than me or I lack permissions.');
-        if (member.id === interaction.user.id) return errorReply('You cannot kick yourself.');
+        if (!member.kickable) {
+            return errorReply('I cannot kick this member. They may have a higher role than me or I lack permissions.');
+        }
+
+        if (member.id === interaction.user.id) {
+            return errorReply('You cannot kick yourself.');
+        }
 
         try {
             await member.kick(reason);
@@ -62,6 +81,7 @@ module.exports = {
 
             // Log to mod-log channel
             const logChannel = interaction.guild.channels.cache.get(LOG_CHANNEL_ID);
+
             if (logChannel) {
                 const logContainer = new ContainerBuilder().addTextDisplayComponents(
                     new TextDisplayBuilder().setContent(
@@ -90,7 +110,11 @@ module.exports = {
                 )
             );
 
-            await interaction.reply({ components: [container], flags: MessageFlags.IsComponentsV2 });
+            await interaction.reply({
+                components: [container],
+                flags: MessageFlags.IsComponentsV2,
+            });
+
         } catch (error) {
             console.error(error);
             await errorReply('Something went wrong while trying to kick that member.');
