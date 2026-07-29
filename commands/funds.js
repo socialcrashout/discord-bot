a55d30d8786cace23227c97bb780290fe881d6ff
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 15:53:13 -0500

    Auto-update: 7/29/2026, 3:53:13 PM

--- commands/funds.js
+++ commands/funds.js
@@ -1,4 +1,8 @@
-const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const {
+    SlashCommandBuilder,
+    MessageFlags,
+} = require('discord.js');
+
 const {
     hasPermission,
     buildNoPermissionContainer,
@@ -7,12 +11,15 @@ const {
     errorMessage,
 } = require('../utils/fundsHelpers');
 
+const ALLOWED_ROLE_ID = 'ROLE_ID_HERE'; // Replace with the role ID that can use /funds
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('funds')
         .setDescription('Check Roblox group funds and pending revenue.'),
 
     execute: async (interaction, client) => {
+        // Existing permission check
         if (!hasPermission(interaction.member)) {
             return interaction.reply({
                 components: [buildNoPermissionContainer()],
@@ -20,18 +27,33 @@ module.exports = {
             });
         }
 
-        await interaction.reply({ content: '💸 Fetching group funds...', ephemeral: true });
+        // Role restriction
+        if (!interaction.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+            return interaction.reply({
+                content: 'You do not have the required role to use this command.',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
+
+        await interaction.reply({
+            content: '💸 Fetching group funds...',
+            ephemeral: true,
+        });
 
         const groupId = process.env.GROUP_ID;
+
         try {
             const data = await fetchFundsData(groupId);
+
             await interaction.editReply({
                 content: null,
                 components: [buildFundsContainer(data)],
                 flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
             });
         } catch (err) {
-            await interaction.editReply({ content: errorMessage(err) });
+            await interaction.editReply({
+                content: errorMessage(err),
+            });
         }
     },
 };
