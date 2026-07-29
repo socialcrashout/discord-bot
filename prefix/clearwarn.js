08d5ffbbefdf6c5f83e556095503ca617ef41fd1
socialcrashout <phantomreaper875@gmail.com>
2026-07-29 16:26:51 -0500

    Auto-update: 7/29/2026, 4:26:51 PM

--- prefix/clearwarn.js
+++ prefix/clearwarn.js
@@ -1,5 +1,4 @@
 const {
-    PermissionFlagsBits,
     ContainerBuilder,
     TextDisplayBuilder,
     MessageFlags,
@@ -8,14 +7,22 @@ const {
 const { removeWarningByCase } = require('../utils/warnings');
 
 const LOG_CHANNEL_ID = '1506450870269906944';
-const ALLOWED_ROLE_ID = '1504311819458580531,1504313264576925757,1504312910862880879,1504320706341502996'; // Replace with the role ID that can use -clearwarn
+
+const ALLOWED_ROLE_IDS = [
+    '1504311819458580531',
+    '1504313264576925757',
+    '1504312910862880879',
+    '1504320706341502996'
+];
 
 module.exports = {
     name: 'clearwarn',
     description: 'Remove a specific warning by its case number',
+
     // Usage: -clearwarn <caseNumber>
 
     async execute(message, args) {
+
         const errorReply = (text) => message.reply({
             components: [
                 new ContainerBuilder().addTextDisplayComponents(
@@ -25,13 +32,8 @@ module.exports = {
             flags: MessageFlags.IsComponentsV2,
         });
 
-        // Permission check
-        if (!message.member.permissions.has(PermissionFlagsBits.ModerateMembers)) {
-            return errorReply('You do not have permission to clear warnings.');
-        }
-
         // Role restriction
-        if (!message.member.roles.cache.has(ALLOWED_ROLE_ID)) {
+        if (!message.member.roles.cache.some(role => ALLOWED_ROLE_IDS.includes(role.id))) {
             return errorReply('You do not have the required role to use this command.');
         }
 
@@ -58,16 +60,17 @@ module.exports = {
         const logChannel = message.guild.channels.cache.get(LOG_CHANNEL_ID);
 
         if (logChannel) {
-            const logContainer = new ContainerBuilder().addTextDisplayComponents(
-                new TextDisplayBuilder().setContent(
-                    `## <:ShieldCheck:1502514212168274061> Clearwarn Command Used!\n` +
-                    `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
-                    `**<:person:1502514200705105981> Warning Removed:** Case #${caseNumber} (was ${removed.userTag})\n` +
-                    `**<:Comment:1502512880493400196> Original Reason:** ${removed.reason}\n` +
-                    `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
-                    `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
-                )
-            );
+            const logContainer = new ContainerBuilder()
+                .addTextDisplayComponents(
+                    new TextDisplayBuilder().setContent(
+                        `## <:ShieldCheck:1502514212168274061> Clearwarn Command Used!\n` +
+                        `-# **<:sig:1502514350014070795> Used By:** ${message.author}\n` +
+                        `**<:person:1502514200705105981> Warning Removed:** Case #${caseNumber} (was ${removed.userTag})\n` +
+                        `**<:Comment:1502512880493400196> Original Reason:** ${removed.reason}\n` +
+                        `**<:Dot:1502513706347528213> Channel:** ${message.channel}\n` +
+                        `**<:Calendar:1502513561866473734> Timestamp:** <t:${Math.floor(Date.now() / 1000)}:F>`
+                    )
+                );
 
             await logChannel.send({
                 components: [logContainer],
@@ -76,14 +79,15 @@ module.exports = {
             });
         }
 
-        const container = new ContainerBuilder().addTextDisplayComponents(
-            new TextDisplayBuilder().setContent(
-                `**Warning Cleared**\n` +
-                `**Case #:** ${caseNumber}\n` +
-                `**User:** ${removed.userTag}\n` +
-                `**Original Reason:** ${removed.reason}`
-            )
-        );
+        const container = new ContainerBuilder()
+            .addTextDisplayComponents(
+                new TextDisplayBuilder().setContent(
+                    `**Warning Cleared**\n` +
+                    `**Case #:** ${caseNumber}\n` +
+                    `**User:** ${removed.userTag}\n` +
+                    `**Original Reason:** ${removed.reason}`
+                )
+            );
 
         await message.channel.send({
             components: [container],
