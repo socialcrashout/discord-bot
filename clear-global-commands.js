7b464398e15a0f8b27fc483497b3dd90b69e29dd
socialcrashout <phantomreaper875@gmail.com>
2026-08-01 13:30:47 -0500

    f

---
+++ clear-global-commands.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const { DISCORD_TOKEN, CLIENT_ID } = process.env;
+
+const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
+
+rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] })
+    .then(() => console.log('Cleared all global commands.'))
+    .catch(console.error);
